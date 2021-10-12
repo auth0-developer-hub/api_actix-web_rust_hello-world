c1a6c25c0f7f5fcc36213056cc61a92b5bdc62ff
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-10-12 11:21:00 -0500

    Secure the API

--- src/api/messages/handlers.rs
+++ src/api/messages/handlers.rs
@@ -1,15 +1,16 @@
 use super::types::Message;
+use crate::extractors::Claims;
 use actix_web::{get, web, Responder};
 
 #[get("/admin")]
-pub async fn admin() -> impl Responder {
+pub async fn admin(_claims: Claims) -> impl Responder {
     web::Json(Message {
         text: "The API successfully recognized you as an admin.".to_string(),
     })
 }
 
 #[get("/protected")]
-pub async fn protected() -> impl Responder {
+pub async fn protected(_claims: Claims) -> impl Responder {
     web::Json(Message {
         text: "The API successfully validated your access token.".to_string(),
     })
