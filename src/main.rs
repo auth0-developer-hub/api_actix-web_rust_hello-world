40ba8b1531e5c7091dd7a8883fd2b273993b3f65
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-10-12 20:40:51 -0500

    Add default_service to handle 404 errors

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,10 @@ async fn admin() -> impl Responder {
     web::Json(Response { message: "The API successfully recognized you as an admin." })
 }
 
+async fn not_found() -> impl Responder {
+    web::HttpResponse::NotFound().json(Response { message: "Not found" })
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -28,6 +32,7 @@ async fn main() -> std::io::Result<()> {
             .service(public)
             .service(protected)
             .service(admin)
+            .default_service(web::to(not_found))
     })
     .bind("127.0.0.1:6060")?
     .run()
