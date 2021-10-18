85a1bde87b972d4547f8ceffc2efe27a10f8d2e2
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-10-18 10:08:23 -0500

    Run `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 struct Config {
-    #[serde(default="default_port")]
+    #[serde(default = "default_port")]
     port: u16,
 }
 
@@ -19,17 +19,23 @@ fn default_port() -> u16 {
 
 #[get("/api/messages/public")]
 async fn public() -> impl Responder {
-    web::Json(Response { message: "The API doesn't require an access token to share this message." })
+    web::Json(Response {
+        message: "The API doesn't require an access token to share this message.",
+    })
 }
 
 #[get("/api/messages/protected")]
 async fn protected() -> impl Responder {
-    web::Json(Response { message: "The API successfully validated your access token." })
+    web::Json(Response {
+        message: "The API successfully validated your access token.",
+    })
 }
 
 #[get("/api/messages/admin")]
 async fn admin() -> impl Responder {
-    web::Json(Response { message: "The API successfully recognized you as an admin." })
+    web::Json(Response {
+        message: "The API successfully recognized you as an admin.",
+    })
 }
 
 #[get("/500")]
@@ -37,14 +43,21 @@ async fn error_500() -> impl Responder {
     web::HttpResponse::InternalServerError()
 }
 
-fn internal_error<B>(mut res: dev::ServiceResponse<B>) -> Result<middleware::errhandlers::ErrorHandlerResponse<B>> {
-    res.headers_mut().insert(http::header::CONTENT_TYPE, http::HeaderValue::from_static("application/json"));
+fn internal_error<B>(
+    mut res: dev::ServiceResponse<B>,
+) -> Result<middleware::errhandlers::ErrorHandlerResponse<B>> {
+    res.headers_mut().insert(
+        http::header::CONTENT_TYPE,
+        http::HeaderValue::from_static("application/json"),
+    );
     // TODO: serialize Response in the response body
     Ok(middleware::errhandlers::ErrorHandlerResponse::Response(res))
 }
 
 async fn not_found() -> impl Responder {
-    web::HttpResponse::NotFound().json(Response { message: "Not found" })
+    web::HttpResponse::NotFound().json(Response {
+        message: "Not found",
+    })
 }
 
 #[actix_web::main]
@@ -57,7 +70,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .wrap(
                 middleware::errhandlers::ErrorHandlers::new()
-                    .handler(http::StatusCode::INTERNAL_SERVER_ERROR, internal_error)
+                    .handler(http::StatusCode::INTERNAL_SERVER_ERROR, internal_error),
             )
             .wrap(cors)
             .service(public)
