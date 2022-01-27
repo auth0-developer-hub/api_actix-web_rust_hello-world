248e0910d95c7ecfc145a791925de433d61f8ea8
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-01-27 14:44:59 -0500

    Update response payload / Dockerize the application

--- src/api/messages/handlers.rs
+++ src/api/messages/handlers.rs
@@ -1,15 +1,17 @@
-use super::types::Message;
+use super::types::{Message, Metadata};
 use crate::{extractors::Claims, types::ErrorMessage};
-use actix_web::{get, web, Responder, HttpResponse};
+use actix_web::{get, web, HttpResponse, Responder};
 use std::collections::HashSet;
 
 #[get("/admin")]
 pub async fn admin(claims: Claims) -> impl Responder {
     if claims.validate_permissions(&HashSet::from(["read:admin-messages".to_string()])) {
         Ok(web::Json(Message {
-            api: "api_actix-web_rust_hello-world".to_string(),
-            branch: "basic-role-based-access-control".to_string(),
-            text: "The secured API requires a valid access token and the read:admin-messages permission to share this admin message.".to_string(),
+            metadata: Metadata {
+                api: "api_actix-web_rust_hello-world".to_string(),
+                branch: "basic-role-based-access-control".to_string(),
+            },
+            text: "This is an admin message.".to_string(),
         }))
     } else {
         Err(HttpResponse::Forbidden().json(ErrorMessage {
@@ -23,17 +25,21 @@ pub async fn admin(claims: Claims) -> impl Responder {
 #[get("/protected")]
 pub async fn protected(_claims: Claims) -> impl Responder {
     web::Json(Message {
-        api: "api_actix-web_rust_hello-world".to_string(),
-        branch: "basic-role-based-access-control".to_string(),
-        text: "The secured API requires a valid access token to share this protected message.".to_string(),
+        metadata: Metadata {
+            api: "api_actix-web_rust_hello-world".to_string(),
+            branch: "basic-role-based-access-control".to_string(),
+        },
+        text: "This is a protected message.".to_string(),
     })
 }
 
 #[get("/public")]
 pub async fn public() -> impl Responder {
     web::Json(Message {
-        api: "api_actix-web_rust_hello-world".to_string(),
-        branch: "basic-role-based-access-control".to_string(),
-        text: "The secured API doesn't require an access token to share this public message.".to_string(),
+        metadata: Metadata {
+            api: "api_actix-web_rust_hello-world".to_string(),
+            branch: "basic-role-based-access-control".to_string(),
+        },
+        text: "This is a public message.".to_string(),
     })
 }
--- src/types.rs
+++ src/types.rs
@@ -2,8 +2,14 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct Config {
+    #[serde(default = "default_host")]
+    pub host: String,
     pub port: u16,
     pub client_origin_url: String,
+}
+
+fn default_host() -> String {
+    "127.0.0.1".to_string()
 }
 
 impl Default for Config {
