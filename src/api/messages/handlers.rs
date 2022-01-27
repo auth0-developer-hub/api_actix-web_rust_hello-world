ee697c38948da73033940991930857a1f6a43d2e
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-01-27 14:45:27 -0500

    Update response payload / Dockerize application

--- src/api/messages/handlers.rs
+++ src/api/messages/handlers.rs
@@ -1,29 +1,35 @@
-use super::types::Message;
+use super::types::{Message, Metadata};
 use actix_web::{get, web, Responder};
 
 #[get("/admin")]
 pub async fn admin() -> impl Responder {
     web::Json(Message {
-        api: "api_actix-web_rust_hello-world".to_string(),
-        branch: "starter".to_string(),
-        text: "The starter API doesn't require an access token to share this admin message.".to_string(),
+        metadata: Metadata {
+            api: "api_actix-web_rust_hello-world".to_string(),
+            branch: "starter".to_string(),
+        },
+        text: "This is an admin message.".to_string(),
     })
 }
 
 #[get("/protected")]
 pub async fn protected() -> impl Responder {
     web::Json(Message {
-        api: "api_actix-web_rust_hello-world".to_string(),
-        branch: "starter".to_string(),
-        text: "The starter API doesn't require an access token to share this protected message.".to_string(),
+        metadata: Metadata {
+            api: "api_actix-web_rust_hello-world".to_string(),
+            branch: "starter".to_string(),
+        },
+        text: "This is a protected message.".to_string(),
     })
 }
 
 #[get("/public")]
 pub async fn public() -> impl Responder {
     web::Json(Message {
-        api: "api_actix-web_rust_hello-world".to_string(),
-        branch: "starter".to_string(),
-        text: "The starter API doesn't require an access token to share this public message.".to_string(),
+        metadata: Metadata {
+            api: "api_actix-web_rust_hello-world".to_string(),
+            branch: "starter".to_string(),
+        },
+        text: "This is a public message.".to_string(),
     })
 }
