a67927e5be19afbede1407f84ab4ef9d304bae83
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-01-28 16:00:31 -0500

    Update response payload / Dockerize the application (#8)

--- src/api/messages/types.rs
+++ src/api/messages/types.rs
@@ -2,7 +2,12 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Message {
+    pub metadata: Metadata,
+    pub text: String,
+}
+
+#[derive(Serialize)]
+pub struct Metadata {
     pub api: String,
     pub branch: String,
-    pub text: String,
 }
